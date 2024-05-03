af6c15f5e2238df36e7c2ffed8a5cedcd4992f02
Zuk's <luiszksaturno@gmail.com>
2024-05-03 12:28:55 -0300

    chore:finalização projt

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-form.addEventListener('submit',function(e){
-    e.preventDefault();
+const form = document.getElementById('formvalidador');
+const valorCampoA = document.getElementById('campa');
+const valorCampoB = document.getElementById('campb');
 
 
+function validaCampo(primeironumero, segundonumero){
+    return primeironumero.value < segundonumero.value;
+}
 
 
+form.addEventListener('submit', function(e){
+    e.preventDefault()
 
+    const msgerror =document.getElementById('erro')
+    const msgsucess =document.getElementById('acerto')
+
+    if (validaCampo(valorCampoA,valorCampoB)){
+        valorCampoA.value = '';
+        valorCampoB.value ='';
+
+        msgsucess.classList.add('messagesucesso')
+        msgerror.classList.remove('errormsg')
+    }else{
+        msgsucess.classList.remove('messagesucesso')
+        msgerror.classList.add('errormsg')
+    }
 })
