422ad1d2e01a8f4a2d3f9638d04273e8c6dba6f9
Zuk's <luiszksaturno@gmail.com>
2024-07-02 23:27:45 -0300

    chore: functions

--- projeto-2/main.js
+++ projeto-2/main.js
@@ -1,18 +1,25 @@
 const form = document.getElementById('formatividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="emoji feliz">';
 const imgReprovado ='<img src="./images/reprovado.png" alt="emoji triste">';
-
+const atividades=[];
+const notas=[];
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     adicionaLinha()
+    atualizaTabela()
+    atualizamediafinal()
+    calculamediafinal()
 })
 
 function adicionaLinha(){
     const inputNome = document.getElementById('nome-atividade');
     const inputNota = document.getElementById('nota-atividade');
+
+    atividades.push(inputNome.value)
+    notas.push(parseFloat(inputNota.value))
 
     let linha = `<tr>`
     linha += `<td>${inputNome.value}</td>`
@@ -25,7 +32,20 @@ function adicionaLinha(){
     inputNome.value=''
     inputNota.value=''
 }
+
 function atualizaTabela(){
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML= linhas;
+}
+
+function atualizamediafinal(){
+    const mediafinal=calculamediafinal()
+    console.log(somaDasNotas)
+}
+function calculamediafinal(){
+    let somaDasNotas=0;
+    for (let i= 0; i <notas.length; i++)
+        somaDasNotas += notas[i]
+    return somaDasNotas / notas.length;
+    
 }
